23fb1c4e250558ec2cba3e991115221eea8321c0
hieugiddy <hieugiddy.pro@gmail.com>
2021-08-31 18:46:49 +0700

    Setup Basic

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\Task\TaskInterface;
+use App\Http\Requests\CreateTask;
+use App\Http\Requests\UpdateTask;
+
+class TaskController extends Controller{
+    protected $taskRepo;
+
+    function __construct(TaskInterface $taskInterface){
+        $this->taskRepo=$taskInterface;
+    }
+
+    function index(){
+        return view('home');
+    }
+
+    function viewThemTask(){
+        return view('them');
+    }
+
+    function new(CreateTask $request){
+    }
+
+    function viewSuaTask($id){
+        return view('sua');
+    }
+
+    function update(UpdateTask $request,$id){
+    }
+
+    function delete($id){
+        echo 'Xoa';
+    }
+}
