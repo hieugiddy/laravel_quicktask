f6bd267c649b95cd59400822fad5f15374e38c73
hieugiddy <hieugiddy.pro@gmail.com>
2021-09-07 16:20:58 +0700

    static page

---
+++ resources/views/sua.blade.php
@@ -0,0 +1,19 @@
+@extends('master')
+@section('title','Sua Task')
+
+@section('content')
+<nav class="breadcrumb">
+    <a class="breadcrumb-item" href="{{ route('home') }}">Home</a>
+    <a class="breadcrumb-item" href="{{ route('home') }}">Task</a>
+    <span class="breadcrumb-item active">Chỉnh sửa Task</span>
+</nav>
+<form method="post">
+    <input type="hidden" name="ID" value=""/>
+    
+    <label for="">Nhập nội dung vào ô bên dưới</label>
+    <div class="input-group mb-3">
+        <input type="text" class="col-md-4 form-control mr-3" value="">
+        <button type="submit" class="btn btn-success">Lưu</button>
+    </div>
+</form>
+@endsection
---
+++ resources/views/them.blade.php
@@ -0,0 +1,21 @@
+@extends('master')
+@section('title','Them Task')
+
+@section('content')
+<nav class="breadcrumb">
+    <a class="breadcrumb-item" href="{{ route('home') }}">Home</a>
+    <a class="breadcrumb-item" href="{{ route('home') }}">Task</a>
+    <span class="breadcrumb-item active">Thêm Task</span>
+</nav>
+<form method="post">
+    <label for="">Nhập nội dung vào ô bên dưới</label>
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">#</span>
+        </div>
+        <input type="text" class="col-md-4 form-control mr-3">
+        <button type="submit" class="btn btn-success">Thêm</button>
+    </div>
+
+</form>
+@endsection
